bd6d3ba3d5717e719437d0f6085a150e235f1446
thejas <thejasgajula@gmail.com>
2026-02-11 12:05:20 +0530

    cors

--- agents/intake_agent/src/app.py
+++ agents/intake_agent/src/app.py
@@ -38,7 +38,8 @@ def create_app() -> FastAPI:
     app.add_middleware(
         CORSMiddleware,
         allow_origins=[
-            "http://localhost:5173",  # Vite dev
+            "http://localhost:5173", 
+            "https://agenticaipaves.netlify.app/" # Vite dev
         ],
         allow_credentials=True,
         allow_methods=["*"],        # enables OPTIONS
